2bc1d585f07718a8917e75432860ebcb6136fd5d
Ted <tdey@tdey.org>
2012-12-09 22:25:41 +0100

    new core

---
+++ MYN/modules/myncfg.py
@@ -0,0 +1,17 @@
+# -*- coding:Utf-8 -*-
+#
+#	Variables pseudo-globales
+#
+
+import os
+import string
+import glob
+import time
+
+def db_vars():
+	db_host = "82.66.164.136"
+	db_name = "myn"
+	db_user = "admin"
+	db_passwd = "P666ingo"
+
+
---
+++ MYN/myn.py
@@ -0,0 +1,30 @@
+# -*- coding:Utf-8 -*-
+#
+# Fichier principal de l'application M.Y.N.
+#
+
+import os
+import string
+import glob
+import time
+from myncfg import *
+
+def main():
+	print ("db_user")
+
+
+#def schedule():
+	
+
+#def ordonn():
+	
+
+#def to_db():
+	
+
+
+
+
+if __name__ == '__main__':
+	main()
+
